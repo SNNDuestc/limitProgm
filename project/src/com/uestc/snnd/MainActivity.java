d103e9b30d99026f1c69f20385b50e217c45b44f
Jonarain <354392089@qq.com>
2014-06-06 10:01:39 +0800

    Signed-off-by: Jonarain <354392089@qq.com>

--- project/src/com/uestc/snnd/MainActivity.java
+++ project/src/com/uestc/snnd/MainActivity.java
@@ -5,11 +5,18 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	
+	int a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+        
+        
+       a= 1+1;
     }
 
 
